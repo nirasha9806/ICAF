06075a22862ac2321e75f2edafa36bf5c3c05bc7
Nirasha Chamudini <nirasha@dmsswe.com>
2021-04-27 00:33:30 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require("path");
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//BodyParser Middleware
+app.use(bodyParser.json());
+
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//connect to Mongo
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+    
+    const port  = process.env.PORT || 5000;
+
+
+    app.listen(port, () => console.log('Server started on port ' +port));
