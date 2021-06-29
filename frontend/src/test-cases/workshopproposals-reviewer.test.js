da3a090f552ba3fcc3927ed30255c334377d3985
Kaveena99 <kaveenadewminijayasekara@gmail.com>
2021-06-29 19:54:49 +0530

    test cases are added

---
+++ frontend/src/test-cases/workshopproposals-reviewer.test.js
@@ -0,0 +1,6 @@
+describe('../test-cases/components/workshopproposals-reviewer-component/ViewProposals', () => {
+    it('Should be true', () => {
+       const test = true;
+       expect(test).toBe(true);
+    });
+ });
