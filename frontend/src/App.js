2e95b9bf059e5f338066e5e2ade956e540148e9c
Nirasha Chamudini <nirasha@dmsswe.com>
2021-06-29 22:16:06 +0530

    Researcher Signup frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import DocumentUpload from './components/document-upload-component/DocumentUpload';
+=======
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+// nirasha
+import SignUpGenereal from './components/signup-component/SignUpGeneral';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <Router>
       {/* nirasha */}
+<<<<<<< Updated upstream
       <Route path='/documentUpload' component={DocumentUpload} />
+=======
+      <Route path='/signup' component={SignUpGenereal} />
+
+
+
+
+    
+      {/* //kaveena */}
+      <Route  path="/attendeePayment" component={AttendeePayment} />
+      <Route  path="/researchPapers" component={ResearchPaperTable} />
+      <Route  path="/workshopProposals" component={WorkshopProposalsTable} />
+
+
+
+
+
+      {/* sanduni */}
+
+
+
+
+
+
+>>>>>>> Stashed changes
     </Router>
   );
 }
