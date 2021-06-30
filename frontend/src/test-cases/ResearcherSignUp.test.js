59ddf45c6f1476c2a247b44e9418cece095372bf
Nirasha Chamudini <nirasha@dmsswe.com>
2021-06-30 22:43:07 +0530

    Test Cases Added

---
+++ frontend/src/test-cases/ResearcherSignUp.test.js
@@ -0,0 +1,6 @@
+describe('../test-cases/components/signup-component/ResearcherSignUpModal', () => {
+    it('Should be true', () => {
+       const test = true;
+       expect(test).toBe(true);
+    });
+ });
